524e37907bbed5b373f597b4474a4a61c7cfba52
Frank Simon <f.m.sim@t-online.de>
2018-03-21 22:39:04 +0100

    modified admin navigation, added admin includes folder

--- includes/navigation.php
+++ includes/navigation.php
@@ -21,10 +21,11 @@
             echo "<li><a href='#'>{$cat_title}</a></li>";
           }
         ?>
-      <!--
+
         <li>
-          <a href="#">About</a>
+          <a href="admin">Admin</a>
         </li>
+      <!--
         <li>
           <a href="#">Services</a>
         </li>
