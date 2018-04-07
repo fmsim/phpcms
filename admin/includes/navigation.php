d09b7507d31fd7f391ee2005e84d4a80c71158d3
Frank Simon <f.m.sim@t-online.de>
2018-04-07 20:54:37 +0200

    modified admin navigation

--- admin/includes/navigation.php
+++ admin/includes/navigation.php
@@ -7,100 +7,12 @@
       <span class="icon-bar"></span>
       <span class="icon-bar"></span>
     </button>
-    <a class="navbar-brand" href="index.html">SB Admin</a>
+    <a class="navbar-brand" href="index.html">CMS Admin</a>
   </div> <!-- .navbar-header -->
     <!-- Top Menu Items -->
   <ul class="nav navbar-right top-nav">
     <li><a href="../index.php">HOME SITE</a></li>
-    <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="fa fa-envelope"></i>
-        <b class="caret"></b>
-      </a>
-        <ul class="dropdown-menu message-dropdown">
-          <li class="message-preview">
-            <a href="#">
-              <div class="media">
-                  <span class="pull-left">
-                      <img class="media-object" src="http://placehold.it/50x50" alt="">
-                  </span>
-                  <div class="media-body">
-                    <h5 class="media-heading">
-                      <strong>John Smith</strong>
-                    </h5>
-                    <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                  </div> <!-- .media-body -->
-              </div> <!-- .media -->
-            </a>
-          </li>
-          <li class="message-preview">
-            <a href="#">
-              <div class="media">
-                  <span class="pull-left">
-                      <img class="media-object" src="http://placehold.it/50x50" alt="">
-                  </span>
-                  <div class="media-body">
-                    <h5 class="media-heading">
-                      <strong>John Smith</strong>
-                    </h5>
-                    <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                    <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                  </div> <!-- .media-body -->
-              </div> <!-- .media -->
-            </a>
-          </li>
-          <li class="message-preview">
-            <a href="#">
-              <div class="media">
-                <span class="pull-left">
-                  <img class="media-object" src="http://placehold.it/50x50" alt="">
-                </span>
-                <div class="media-body">
-                  <h5 class="media-heading">
-                    <strong>John Smith</strong>
-                  </h5>
-                  <p class="small text-muted"><i class="fa fa-clock-o"></i> Yesterday at 4:32 PM</p>
-                  <p>Lorem ipsum dolor sit amet, consectetur...</p>
-                </div> <!-- .media-body -->
-              </div> <!-- .media -->
-            </a>
-          </li>
-          <li class="message-footer">
-            <a href="#">Read All New Messages</a>
-          </li>
-        </ul>
-    </li>
-    <li class="dropdown">
-      <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="fa fa-bell"></i>
-        <b class="caret"></b>
-      </a>
-      <ul class="dropdown-menu alert-dropdown">
-        <li>
-          <a href="#">Alert Name <span class="label label-default">Alert Badge</span></a>
-        </li>
-        <li>
-          <a href="#">Alert Name <span class="label label-primary">Alert Badge</span></a>
-        </li>
-        <li>
-          <a href="#">Alert Name <span class="label label-success">Alert Badge</span></a>
-        </li>
-        <li>
-          <a href="#">Alert Name <span class="label label-info">Alert Badge</span></a>
-        </li>
-        <li>
-          <a href="#">Alert Name <span class="label label-warning">Alert Badge</span></a>
-        </li>
-        <li>
-          <a href="#">Alert Name <span class="label label-danger">Alert Badge</span></a>
-        </li>
-        <li class="divider"></li>
-        <li>
-          <a href="#">View All</a>
-        </li>
-      </ul> <!-- .dropdown-menu alert-dropdown -->
-    </li>
+
     <li class="dropdown">
       <a href="#" class="dropdown-toggle" data-toggle="dropdown">
         <i class="fa fa-user"></i> John Smith
@@ -109,12 +21,6 @@
       <ul class="dropdown-menu">
         <li>
           <a href="#"><i class="fa fa-fw fa-user"></i> Profile</a>
-        </li>
-        <li>
-          <a href="#"><i class="fa fa-fw fa-envelope"></i> Inbox</a>
-        </li>
-        <li>
-          <a href="#"><i class="fa fa-fw fa-gear"></i> Settings</a>
         </li>
         <li class="divider"></li>
         <li>
@@ -130,36 +36,35 @@
         <a href="index.html"><i class="fa fa-fw fa-dashboard"></i> Dashboard</a>
       </li>
       <li>
-        <a href="charts.html"><i class="fa fa-fw fa-bar-chart-o"></i> Charts</a>
-      </li>
-      <li>
-        <a href="tables.html"><i class="fa fa-fw fa-table"></i> Tables</a>
-      </li>
-      <li>
-        <a href="forms.html"><i class="fa fa-fw fa-edit"></i> Forms</a>
-      </li>
-      <li>
-        <a href="bootstrap-elements.html"><i class="fa fa-fw fa-desktop"></i> Bootstrap Elements</a>
-      </li>
-      <li>
-        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Bootstrap Grid</a>
-      </li>
-      <li>
-        <a href="javascript:;" data-toggle="collapse" data-target="#demo"><i class="fa fa-fw fa-arrows-v"></i> Dropdown <i class="fa fa-fw fa-caret-down"></i></a>
-        <ul id="demo" class="collapse">
+        <a href="javascript:;" data-toggle="collapse" data-target="#posts-dropdown"><i class="fa fa-fw fa-arrows-v"></i> Posts <i class="fa fa-fw fa-caret-down"></i></a>
+        <ul id="posts-dropdown" class="collapse">
           <li>
-            <a href="#">Dropdown Item</a>
+            <a href="#">View all posts</a>
           </li>
           <li>
-            <a href="#">Dropdown Item</a>
+            <a href="#">Add posts</a>
           </li>
         </ul>
       </li>
+      <li>
+        <a href="bootstrap-grid.html"><i class="fa fa-fw fa-wrench"></i> Categories</a>
+      </li>
       <li class="active">
-        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Blank Page</a>
+        <a href="blank-page.html"><i class="fa fa-fw fa-file"></i> Comments</a>
       </li>
       <li>
-        <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> RTL Dashboard</a>
+        <a href="javascript:;" data-toggle="collapse" data-target="#users-dropdown"><i class="fa fa-fw fa-arrows-v"></i> Users<i class="fa fa-fw fa-caret-down"></i></a>
+        <ul id="users-dropdown" class="collapse">
+          <li>
+            <a href="#">View all posts</a>
+          </li>
+          <li>
+            <a href="#">Add posts</a>
+          </li>
+        </ul>
+      </li>
+      <li>
+        <a href="index-rtl.html"><i class="fa fa-fw fa-dashboard"></i> Profile</a>
       </li>
     </ul>
   </div> <!-- /.navbar-collapse -->
