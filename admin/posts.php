401dc433d10f1114ffb21a578d99df18fd118e9e
Frank Simon <f.m.sim@t-online.de>
2018-05-29 22:41:06 +0200

    modified posts.php. added add_post.php and view_all_posts.php

--- admin/posts.php
+++ admin/posts.php
@@ -11,42 +11,29 @@
               Welcome to admin
               <small>Author</small>
             </h1>
-            <table class="table table-bordered table-hover">
-              <thead>
-                <tr>
-                  <th>ID</th>
-                  <th>Author</th>
-                  <th>Title</th>
-                  <th>Category</th>
-                  <th>Status</th>
-                  <th>Image</th>
-                  <th>Tags</th>
-                  <th>Comments</th>
-                  <th>Date</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>10</td>
-                  <td>Frank Simon</td>
-                  <td>Bootstrap framework</td>
-                  <td>Bootstrap</td>
-                  <td>Status</td>
-                  <td>Image</td>
-                  <td>Tags</td>
-                  <td>Comments</td>
-                  <td>Date</td>
-                </tr>
-              </tbody>
-            </table>
+            <?php
+              if (isset($_GET['source'])) {
+                $source = $_GET['source'];
+              } else {
+                $source = '';
+              }
+              switch($source) {
+                case 'add_post';
+                  include "includes/add_post.php";
+                  break;
+                case '100';
+                  echo "Nice 100";
+                  break;
+                case '200';
+                  echo "Nice 200";
+                  break;
+                default:
+                  include "includes/view_all_posts.php";
+                  break;
+              }
+            ?>
           </div> <!-- .col-lg-12 -->
         </div> <!-- /.row -->
       </div> <!-- /.container-fluid -->
     </div> <!-- /#page-wrapper -->
 <?php include "includes/admin_footer.php"; ?>
-<?php
-/*
- ?delete= ->is key of associative array
- {$cat_id -> the value}
-*/
-?>
